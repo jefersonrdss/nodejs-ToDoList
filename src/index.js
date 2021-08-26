e5516a1739db4ec8780dbd8d92651db4251d5f80
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-08-25 21:50:05 -0300

    finished create user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = []; // lista de usuarios cadastrados
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+// Cadastrar usuário
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+    const { name, username } = request.body; // dados da requisição
+
+    // objeto do novo usuario
+    const newUser = {
+        id: uuidv4(),
+        name,
+        username,
+        todos: []
+    };
+
+    users.push(newUser); // adicionando novo usuario
+
+    return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
